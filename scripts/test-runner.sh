2fa8e2d69a6a1f7420c2b51518a763980a6dd849
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2017-03-20 19:10:16 -0400

    Make sure we build a binary before running the tests

--- scripts/test-runner.sh
+++ scripts/test-runner.sh
@@ -5,4 +5,6 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && cd .. && pwd )"
 # make sure image is built
 cd $dir && docker build -t alias-test-runner -f spec/Dockerfile .
 
+docker-compose run rust bash -c 'cd /code && cargo build'
+
 docker run -ti -e "APP_ROOT=${dir}" -v ${dir}:/code  -v /var/run/docker.sock:/var/run/docker.sock alias-test-runner bundle exec rspec spec
