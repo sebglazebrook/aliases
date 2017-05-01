89b1a62d24e52ca0842779e39d6fc2c603e5332a
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2017-05-01 05:50:51 +0000

    Using User model instead of username string in DisableUser

--- src/aliases/commands/disable_user.rs
+++ src/aliases/commands/disable_user.rs
@@ -1,14 +1,18 @@
 use aliases::Config;
 use aliases::commands::{CommandResponse, AliasCommand};
+use aliases::repositories::UserRepository;
+use aliases::models::User;
 
 pub struct DisableUser {
-    username: String,
+    user: User,
 }
 
 impl DisableUser {
 
     pub fn new(username: String) -> Self {
-        DisableUser { username: username }
+        DisableUser {
+            user: UserRepository::find_by_name_or_blow(&username),
+        }
     }
 
 }
@@ -17,7 +21,7 @@ impl AliasCommand for DisableUser {
 
     fn execute(&self) -> CommandResponse {
         let mut config = Config::load();
-        config.disable_user(&self.username);
+        config.disable_user(&self.user.name());
         CommandResponse::success()
     }
 }
