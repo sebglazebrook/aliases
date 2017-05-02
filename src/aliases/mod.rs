c843363bef8f2583480f3c21d1ed946f2d62011c
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2017-05-02 13:55:27 +0000

    tell don't ask refactoring on CloneRepo command

--- src/aliases/mod.rs
+++ src/aliases/mod.rs
@@ -4,12 +4,14 @@ pub mod collections;
 pub mod models;
 pub mod factories;
 pub mod builders;
-mod repositories;
 
+mod repositories;
 mod app;
 mod config;
 mod execution_workflow;
+mod git;
 
 pub use self::execution_workflow::ExecutionWorkflow;
 pub use self::config::Config;
 pub use self::app::App;
+pub use self::git::Git;
--- src/aliases/models/user.rs
+++ src/aliases/models/user.rs
@@ -1,9 +1,10 @@
 use std::env;
 use std::path::PathBuf;
 use std::io;
+use std::os::unix;
 
 use aliases::repositories::AliasFileRepository;
-use aliases::Config;
+use aliases::{Config, Git};
 
 #[derive(PartialEq,Eq,Debug,Clone)]
 pub struct User {
@@ -30,10 +31,6 @@ impl User {
 
     pub fn is_enabled(&self) -> bool {
         self.enabled
-    }
-
-    pub fn name(&self) -> String {
-        self.name.clone()
     }
 
     pub fn confirm_name(&self, other_name: &str) -> bool {
@@ -65,6 +62,40 @@ impl User {
 
    pub fn set_priority(&self, position: usize) -> Result<(), String> {
         Config::load().set_user_priority(&self.name, position)
+    }
+
+   pub fn clone_external_repo(&self, url: Option<String>) -> Result<(), String> {
+       let external_url = url.unwrap_or(self.default_external_url());
+       match Git::clone(external_url, &self.output_directory()) {
+           Err(message) => Err(message),
+           Ok(_) => {
+               self.link_aliases_file();
+               Ok(())
+           }
+       }
+   }
+
+   fn default_external_url(&self) -> String {
+       format!("https://github.com/{}/dot-aliases", self.name)
+   }
+
+   fn output_directory(&self) -> String {
+       self.output_base_dir() + "/"  + &self.name
+   }
+
+   fn output_base_dir(&self) -> String {
+       match env::var("HOME") {
+           Err(_) => { String::from("No $HOME environment variable set. Don't know your home directory") },
+           Ok(home_dir) => {
+               format!("{}/.aliases.d/users", home_dir)
+           },
+       }
+   }
+
+    fn link_aliases_file(&self) {
+        let target_file = self.output_directory() + "/.aliases";
+        let destination_file = env::var("HOME").unwrap().to_string() + &self.filename; // TODO handle this better;
+        unix::fs::symlink(target_file, destination_file); // TODO handle the result of this and what about if it's not unix?
     }
 
 }
